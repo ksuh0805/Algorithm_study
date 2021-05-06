5b34b28957dac671d15942089ed1c223060288bd
suhyun <ksuh0805@naver.com>
2021-05-07 02:31:45 +0900

    PS 구명보트

--- com/programmers/구명보트/Solution_구명보트.java
+++ com/programmers/구명보트/Solution_구명보트.java
@@ -10,21 +10,11 @@ public class Solution_구명보트 {
         List<Integer> list = new ArrayList<>();
         for(int w : people) list.add(w);
         Collections.sort(list, Collections.reverseOrder());
-        outer:
         for(int i = 0; i < list.size(); i++) {
         	int w = list.get(i);
         	if(w + list.get(list.size() - 1) <= limit) {
         		list.remove(list.size() - 1);
-//        		answer++;
-//        		continue outer;
         	}
-//        	for(int j = i + 1; j < list.size(); j++) {
-//        		if(w + list.get(j) <= limit) {
-//        			list.remove(j);
-//        			answer++;
-//        			continue outer;
-//        		}
-//        	}
         	answer++;
         }
         return answer;
